0e04181f62574f868ff6c07ec525079762cf90d0
Yogender Kala <yogenderkala1@gmail.com>
2022-07-31 12:18:46 +0530

    added new programs

--- c++/_3_advance_patterns/pattern1.cpp
+++ c++/_3_advance_patterns/pattern1.cpp
@@ -1,5 +1,20 @@
+//Q1. Print following pattern.
+// 12345
+// 1234
+// 123
+// 12
+// 1
 #include<iostream>
 using namespace std;
 int main(){
-
+    int n;
+    cin>>n;
+    for (int i = n; i >=1; i--)
+    {
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
 }
