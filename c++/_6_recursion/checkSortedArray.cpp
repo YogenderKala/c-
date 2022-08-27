d944d860743b5fecb0a1e88272c6428d6c768ff1
Yogender Kala <yogenderkala1@gmail.com>
2022-08-27 14:04:21 +0530

    change

--- c++/_6_recursion/checkSortedArray.cpp
+++ c++/_6_recursion/checkSortedArray.cpp
@@ -1,1 +1,20 @@
 // to check if array is sorted or not
+#include<iostream>
+using namespace std;
+bool checkSort(int arr[],int n){
+    if(n==1){
+        return true;
+    }
+    bool restArr = checkSort(arr+1,n-1);
+    return (arr[0]<arr[1] && restArr);
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int  i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<checkSort(arr,n);
+}
